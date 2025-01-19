5cf3fc8f5ed0bceb6a976880535e3dc55776477c
karim-bahgat <eng.karimbahgat@gmail.com>
2025-01-19 15:44:11 +0200

    add run display

--- __init__.py
+++ __init__.py
@@ -58,6 +58,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: TISConfigEntry) -> bool:
         hass=hass,
         domain=DOMAIN,
         devices_dict=DEVICES_DICT,
+        display_logo="./custom_components/tis_control/logo.png",
     )
     entry.runtime_data = TISData(api=tis_api)
 
@@ -67,6 +68,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: TISConfigEntry) -> bool:
         hass.http.register_view(TISEndPoint(tis_api))
         hass.http.register_view(ScanDevicesEndPoint(tis_api))
         hass.http.register_view(GetKeyEndpoint(tis_api))
+        hass.async_add_executor_job(tis_api.run_display)
     except ConnectionError as e:
         logging.error("error connecting to TIS api %s", e)
         return False
